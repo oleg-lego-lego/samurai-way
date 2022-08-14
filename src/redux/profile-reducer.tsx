3e78889ccaacb5ce8934b3c37a0390fa4a8ad1ab
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-08-14 20:55:28 +0300

    refactoring type in profileReducer

--- src/redux/profile-reducer.tsx
+++ src/redux/profile-reducer.tsx
@@ -1,24 +1,59 @@
-import {ActionsTypes, PostsPropsType, ProfilePageType,} from "./store";
+import {ActionsTypes, PostsPropsType,} from "./store";
 
 const ADD_POST = 'ADD-POST';
 const CHANGE_NEW_TEXT = 'CHANGE-NEW-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 
-
 export type AddPostActionType = ReturnType<typeof addPostActionCreator>
 export type ChangeNewTextType = ReturnType<typeof updateNewPostTextActionCreator>
 export type SetUserProfileActionType = ReturnType<typeof setUserProfileActionCreator>
 
-let initialState: ProfilePageType = {
+export type MyPostsArrayProps = {
+    id: number
+    message: string
+    likesCount: string // fixed string || number
+}
+
+export type ContactsPropsType = {
+    github: string
+    vk: string
+    facebook: string
+    instagram: string
+    twitter: string
+    website: string
+    youtube: string
+    mainLink: string
+}
+
+export type PhotoPropsType = {
+    small: string
+    large: string
+}
+
+export type ProfileType = {
+    userId: number
+    lookingForAJob: boolean
+    lookingForAJobDescription: string
+    fullName: string
+    contacts: ContactsPropsType
+    photos: PhotoPropsType
+    aboutMe: string
+}
+
+let initialState = {
+//let initialState: ProfilePageType = {
     newPostText: '',
     posts: [
         {id: 1, message: 'Hi, how are you?', likesCount: '12'},
         {id: 2, message: 'It is my first post', likesCount: '23'},
-    ],
-    //profile: any ??
+    ]  as Array<MyPostsArrayProps>,
+    profile: null as ProfileType | null,
 }
 
-export const profileReducer = (state = initialState, action: ActionsTypes): ProfilePageType | any => { // fixed any
+export type InitialStateType = typeof initialState
+
+
+export const profileReducer = (state = initialState, action: ActionsTypes): InitialStateType => { // fixed any
     switch (action.type) {
         case ADD_POST: {
             const newPost: PostsPropsType = {
