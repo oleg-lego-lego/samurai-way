bec91750a001e5bc6d8a6fbc15de0706ab891587
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-08-21 15:39:23 +0300

    add NavLink to '/login' in Header components

--- src/componets/Header/Header.tsx
+++ src/componets/Header/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
 export const Header = () => {
     return (
         <header className={s.header}>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/ZDF_logo%21_Logo_2021.svg/1200px-ZDF_logo%21_Logo_2021.svg.png" />
+            <div className={s.loginBLock}>
+                <NavLink to={'/login'}>Login</NavLink>
+            </div>
         </header>
     )
 }
