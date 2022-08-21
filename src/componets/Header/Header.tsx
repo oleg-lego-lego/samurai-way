f8c471e94cb5ef8b186cba17277073005b8ce599
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-08-21 15:45:45 +0300

    created class HeaderContainer

--- src/componets/Header/Header.tsx
+++ src/componets/Header/Header.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import s from './Header.module.css';
 import {NavLink} from "react-router-dom";
 
-export const Header = () => {
+type HeaderPropsType = {
+
+}
+
+
+export const Header = (props: any) => {
     return (
         <header className={s.header}>
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/ZDF_logo%21_Logo_2021.svg/1200px-ZDF_logo%21_Logo_2021.svg.png" />
