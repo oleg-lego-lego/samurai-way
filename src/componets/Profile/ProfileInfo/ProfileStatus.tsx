b4b6a2b1c341e6edc0e6142f187bcec726cd232b
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-09-11 19:46:50 +0300

    refactored ProfileStatus

--- src/componets/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/componets/Profile/ProfileInfo/ProfileStatus.tsx
@@ -9,13 +9,13 @@ export class ProfileStatus extends React.Component<ProfileStatusPropsType> {
         editMode: false
     }
 
-    activateEditMode() {
+    activateEditMode = () => {
         this.setState({
             editMode: true
         })
     }
 
-    deactivateEditMode() {
+    deactivateEditMode = () => {
         this.setState({
             editMode: false
         })
@@ -26,12 +26,12 @@ export class ProfileStatus extends React.Component<ProfileStatusPropsType> {
             <div>
                 {!this.state.editMode &&
                 <div>
-                    <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                    <span onDoubleClick={this.activateEditMode}>{this.props.status}</span>
                 </div>
                 }
                 {this.state.editMode &&
                 <div>
-                    <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
+                    <input autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status}/>
                 </div>
                 }
             </div>
