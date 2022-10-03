361a99faf203be71e06db8bacc2339df7aa6711a
oleg ostrovski <oleg.ostrovski@gmail.com>
2022-10-03 19:27:10 +0300

    fixed logic in MyPosts

--- src/componets/common/FormsControls/FormsControls.tsx
+++ src/componets/common/FormsControls/FormsControls.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import {WrappedFieldProps} from "redux-form";
+import s from './FormsControls.module.css';
 
 
 export const Textarea: React.FC<WrappedFieldProps> = (props) => {
     const {input, meta, children, ...restProps} = props
 
+    const hasError = meta.touched && meta.error
+
+
     return (
-        <div>
-            <textarea {...input} {...props}/>
+        <div className={s.formControl + ' ' + (hasError ? s.error : '')}>
+            <div>
+                <textarea {...input} {...props}/>
+            </div>
+            {hasError && <span>{meta.error}</span>}
         </div>
     )
 }
